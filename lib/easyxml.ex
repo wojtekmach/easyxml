3337dbf18412d65929cd4e53e305a1d4e64476a3
Wojtek Mach <wojtek@wojtekmach.pl>
2021-06-14 23:23:49 +0200

    Improve pretty printing

--- lib/easyxml.ex
+++ lib/easyxml.ex
@@ -1,12 +1,9 @@
-defmodule EasyXML.Node do
+defmodule EasyXML.Doc do
   defstruct [:node]
 
   defimpl Inspect do
-    import Inspect.Algebra
-
-    def inspect(node, _opts) do
-      doc = EasyXML.to_algebra(node)
-      concat(["#EasyXML.Node[", doc, "]"])
+    def inspect(node, opts) do
+      EasyXML.to_algebra(node, opts)
     end
   end
 
@@ -32,7 +29,7 @@ defmodule EasyXML.Node do
             {:ok, value}
 
           [] ->
-            nil
+            :error
 
           nodes ->
             raise "node[#{inspect(key)}] only works on single nodes with text, use EasyXML.xpath/2 for other cases. Got: #{inspect(nodes)}"
@@ -52,7 +49,7 @@ defmodule EasyXML do
     end
 
     [node] = :xmerl_lib.remove_whitespace([node])
-    %EasyXML.Node{node: node}
+    %EasyXML.Doc{node: node}
   end
 
   require Record
@@ -65,7 +62,7 @@ defmodule EasyXML do
     xpath(parse!(xml), path)
   end
 
-  def xpath(%EasyXML.Node{} = node, path) when is_binary(path) do
+  def xpath(%EasyXML.Doc{} = node, path) when is_binary(path) do
     for node <- :xmerl_xpath.string(String.to_charlist(path), node.node) do
       case node do
         binary when is_binary(binary) ->
@@ -78,23 +75,85 @@ defmodule EasyXML do
           List.to_string(value)
 
         xmlElement() = element ->
-          %EasyXML.Node{node: element}
+          %EasyXML.Doc{node: element}
       end
     end
   end
 
-  def dump_to_iodata(%EasyXML.Node{node: xmlElement() = node}) do
+  def dump_to_iodata(%EasyXML.Doc{node: xmlElement() = node}) do
     prolog = xmlAttribute(name: :prolog, value: "<?xml version=\"1.0\" encoding=\"utf-8\"?>")
     :xmerl.export_simple([node], :xmerl_xml, [prolog])
   end
 
-  def to_algebra(%EasyXML.Node{node: xmlElement() = element}) do
-    doc = :xmerl.export_simple([element], :xmerl_xml, [])
-    "<?xml version=\"1.0\"?>" <> rest = IO.iodata_to_binary(doc)
-    rest
+  import Inspect.Algebra
+
+  def to_algebra(%EasyXML.Doc{node: node}, opts) do
+    opts = %{opts | syntax_colors: Keyword.put_new(opts.syntax_colors, :tag, :black)}
+
+    concat([
+      "#EasyXML.Doc[",
+      to_algebra(node, opts),
+      "]"
+    ])
   end
 
-  def to_algebra(%EasyXML.Node{node: xmlAttribute(value: value)}) do
+  def to_algebra(xmlElement(content: []) = element, opts) do
+    open_tag(element, opts, true)
+  end
+
+  def to_algebra(xmlElement(content: [xmlText(value: value)]) = element, opts) do
+    concat([
+      open_tag(element, opts),
+      color(List.to_string(value), :string, opts),
+      close_tag(element, opts)
+    ])
+  end
+
+  def to_algebra(xmlElement(content: content) = element, opts) do
+    container_doc(
+      open_tag(element, opts),
+      content,
+      close_tag(element, opts),
+      opts,
+      &to_algebra/2,
+      break: :strict,
+      separator: ""
+    )
+  end
+
+  def to_algebra(xmlText(value: value), opts) do
     List.to_string(value)
+    |> color(:string, opts)
+  end
+
+  def to_algebra(xmlAttribute(value: value), opts) do
+    List.to_string(value)
+    |> color(:string, opts)
+  end
+
+  def to_algebra(other, _opts) do
+    raise inspect(other)
+  end
+
+  defp open_tag(xmlElement(name: name, attributes: attributes), opts, empty? \\ false) do
+    attributes =
+      for xmlAttribute(name: name, value: value) <- attributes do
+        concat([tag(" #{name}=", opts), color("\"#{value}\"", :string, opts)])
+      end
+      |> concat()
+
+    if empty? do
+      concat([tag("<#{name}", opts), attributes, tag("/>", opts)])
+    else
+      concat([tag("<#{name}", opts), attributes, tag(">", opts)])
+    end
+  end
+
+  defp close_tag(xmlElement(name: name), opts) do
+    color("</#{name}>", :tag, opts)
+  end
+
+  defp tag(doc, opts) do
+    color(doc, :tag, opts)
   end
 end
--- test/easyxml_test.exs
+++ test/easyxml_test.exs
@@ -8,11 +8,15 @@ defmodule EasyXMLTest do
     <points>\
     <point x="1" y="2"/>\
     <point x="3" y="4"/>\
+    <point x="5" y="6"/>\
     </points>\
     """
 
     node = EasyXML.parse!(xml)
+    IO.inspect(node)
     assert node |> EasyXML.dump_to_iodata() |> IO.iodata_to_binary() == xml
-    assert EasyXML.xpath(node, "//point") |> Enum.map(& &1["@x"]) == ["1", "3"]
+    assert EasyXML.xpath(node, "//point") |> Enum.map(& &1["@x"]) == ["1", "3", "5"]
+    refute node["bad"]
+    refute node["@bad"]
   end
 end
